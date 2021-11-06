a2a7cec9ceb4359f0a0cc5bef7861785ddfe7bcf
Ryan-ops68 <72042413+Ryan-ops68@users.noreply.github.com>
2021-11-06 11:18:14 -0400

    Add files via upload

---
+++ activity.py
@@ -0,0 +1,34 @@
+from os import stat
+import pandas as pd
+import csv
+import random
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+import statistics
+df = pd.read_csv("data.csv")
+data = df["average"].tolist()
+def random_set_of_means(counter): 
+    dataset = []
+    for i in range(0,counter):
+        random_index = random.randint(0,len(data)-1)
+        value = data [random_index]
+        dataset.append(value)
+    mean = statistics.mean(dataset)
+    return mean
+def show_fig(mean_list):
+    df = mean_list
+    mean = statistics.mean(df)
+    fig = ff.create_distplot([df],["average"],show_hist=False)
+    fig.add_trace(go.Scatter(x = [mean,mean], y = [0,1], mode = "lines", name = "mean"))
+    fig.show()
+def setup():
+    mean_list = []
+    for i in range (0,1000):
+        set_of_means = random_set_of_means(100)
+        mean_list.append(set_of_means)
+    show_fig(mean_list)
+    mean = statistics.mean(mean_list)
+    print ("Mean of Sampling Distribution is ", mean)
+setup()
+population_mean = statistics.mean(data)
+print ("Population mean", population_mean)
